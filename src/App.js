b689eb7af12b0dd649269351193f53d9f78a64db
khoa <tdkhoa.aqua@gmail.com]>
2023-01-02 00:59:35 -0800

    first'

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './Style/App.css'
+
+import Frame1 from './Frame1.js'
+function App() {
+  return (
+    <div className='mainWrapper'>
+      <Frame1></Frame1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Frame1.js
@@ -0,0 +1,88 @@
+import './Style/Frame1.css'
+function Frame1 () {
+    return(
+        <div id="frame1__wrapper">
+        <div id="myVideo">
+          <div>
+            <div class="shootingStarContainer">
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+            </div>
+            <div class="shootingStarContainer">
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+            </div>
+            <div class="shootingStarContainer">
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+              <div class="shootingStar"></div>
+            </div>
+            <div class="stars"></div>
+            <div class="twinkleMask"></div>
+            <div class="overflow">
+              <div class="twinkleMask2"></div>
+              <div class="clouds"></div>
+            </div>
+            <div class="fogContainer">
+              <div class="fog"></div>
+            </div>
+          </div>
+        </div>
+        <nav id="nav__wrapper">
+          <ul>
+            <a class="nav_link" href="#header__wrapper">
+              <li class="nav__btn">Start</li>
+            </a>
+            <a class="nav_link" href="#main">
+              <li class="nav__btn">Projects</li>
+            </a>
+            <a class="nav_link" href="#footer">
+              <li class="nav__btn">Contact</li>
+            </a>
+          </ul>
+        </nav>
+        <header id="header__wrapper">
+          <div id="center__wrapper">
+            <a href="#main">
+              <div id="center">
+                <div class="center__text title-text2">START</div>
+                <div id="sun">
+                  <div id="earth">
+                    <div id="moon"></div>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div id="heading">
+            <h1 class="header_title title-text" id="frame1_heading"></h1>
+            <p class="header_des title-text2">I'M TRUONG KHOA, UX UI GRAPHIC DESIGNER</p>
+          </div>
+        </header>
+      </div>
+    )
+}
+export default Frame1;
