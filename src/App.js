8a2bc3772c436ab104388cf97bf4796ae5439fe6
Truongkhoa1999 <tdkhoa.aqua@gmail.com>
2023-01-06 14:36:38 +0200

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './Style/App.css'
+import "./Style/App.css";
+import Frame1 from "./Frame1/Frame1.js";
+import Frame2 from "./Frame2/Frame2.js";
+import Frame3 from "./Frame3/Frame3.js";
+import Footer from "./Footer/Footer.js";
 
-import Frame1 from './Frame1.js'
 function App() {
   return (
-    <div className='mainWrapper'>
-      <Frame1></Frame1>
+    <div className="mainWrapper">
+      <Frame1 />
+      <Frame2 />
+      <Frame3 />
+      <Footer />
     </div>
   );
 }
